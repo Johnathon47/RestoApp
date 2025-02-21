53079cc0b52d803a030f90ec72471832e2562afd
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-21 22:54:44 +0300

    feat: add Dish.java, content attribut of dish

---
+++ src/entity/Dish.java
@@ -0,0 +1,10 @@
+package entity;
+
+import java.util.List;
+
+public class Dish {
+    private int id;
+    private String name;
+    private int unitPrice;
+    private List<Ingredient> ingredientList;
+}
