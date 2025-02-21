9d568d3b8b15fda7dcb57bdb73cc49e29af320d6
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-21 23:45:27 +0300

    feat: implement JDBC connection with PostgreSQL

--- src/db/DataSource.java
+++ src/db/DataSource.java
@@ -1,5 +1,26 @@
 package db;
 
-public class DataSource {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DataSource {
+    private final static int defaultPortPsql = 5432;
+    private final String host = System.getenv("DATABASE_HOST");
+    private final String user = System.getenv("DATABASE_USER");
+    private final String loginPassword = System.getenv("LOGIN_PASSWORD_DATABASE");
+    private final String nameDatabase = System.getenv("DATABASE_NAME");
+    private final String jdbcUrl;
+
+    public DataSource() {
+        jdbcUrl = "jdbc:postgresql://"+host+":"+defaultPortPsql+"/"+nameDatabase;
+    }
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(jdbcUrl, user, loginPassword);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
