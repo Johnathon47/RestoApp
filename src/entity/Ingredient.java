e9169da95dec15c4cf53e2b64fc493f422149c96
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-21 22:58:34 +0300

    feat: add Ingredient.java, content attribut of ingredient

---
+++ src/entity/Ingredient.java
@@ -0,0 +1,11 @@
+package entity;
+
+import java.util.Date;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private int unitPrice;
+    private Unit unit;
+    private Date updateDateTime;
+}
