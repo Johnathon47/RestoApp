1f68b2e4d5b07405e5be44a977622872e33519aa
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-22 00:22:31 +0300

    feat: command sql create enum unit and create table ingredient

---
+++ src/db/migration/1_create_table_ingredient.sql
@@ -0,0 +1,17 @@
+do
+$$
+    begin
+        if not exists(SELECT FROM pg_type WHERE typname = 'unit') then
+            CREATE TYPE "unit" AS ENUM ('G', 'L', 'U');
+        end if;
+    end
+$$;
+
+CREATE TABLE if not exists ingredient
+    (
+        id INT PRIMARY KEY,
+        name VARCHAR(255),
+        unit_price INT,
+        unit unit,
+        update_datetime DATE
+    );
