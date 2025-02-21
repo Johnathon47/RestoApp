a9e5b59f38f54477b9720324da70d40616250432
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-22 01:06:27 +0300

    fix: request sql for primary key combined

--- src/db/migration/3_create_table_dishIngredient.sql
+++ src/db/migration/3_create_table_dishIngredient.sql
@@ -1,9 +1,10 @@
 CREATE TABLE if not exists dish_ingredient
     (
-        id_dish INT PRIMARY KEY,
-        id_ingredient INT PRIMARY KEY,
+        id_dish INT,
+        id_ingredient INT,
         required_quality NUMERIC,
         unit unit,
+        PRIMARY KEY (id_dish, id_ingredient),
         FOREIGN (id_dish) REFERENCES dish(id),
         FOREIGN (id_ingredient) REFERENCES ingredient(id)
     );
