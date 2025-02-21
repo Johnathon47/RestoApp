a9df7faf0e3480a19dc6a562234107c848c72e2d
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-22 01:08:51 +0300

    fix: forgot word KEY on my FOREIGN(request sql)

--- src/db/migration/3_create_table_dishIngredient.sql
+++ src/db/migration/3_create_table_dishIngredient.sql
@@ -5,6 +5,6 @@ CREATE TABLE if not exists dish_ingredient
         required_quality NUMERIC,
         unit unit,
         PRIMARY KEY (id_dish, id_ingredient),
-        FOREIGN (id_dish) REFERENCES dish(id),
-        FOREIGN (id_ingredient) REFERENCES ingredient(id)
+        FOREIGN KEY (id_dish) REFERENCES dish(id),
+        FOREIGN KEY (id_ingredient) REFERENCES ingredient(id)
     );
