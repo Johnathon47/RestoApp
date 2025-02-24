6addb2f022bda796f1bf98179aa2a6de028ac262
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-24 13:37:38 +0300

    feat: change type of unitPrice in BigDecimal and change name's scope in protected (add constructor, getter, setter, hash code, equals and toString)

--- src/entity/Ingredient.java
+++ src/entity/Ingredient.java
@@ -1,11 +1,78 @@
 package entity;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 
 public class Ingredient {
     private int id;
-    private String name;
-    private int unitPrice;
+    protected String name;
+    private BigDecimal unitPrice;
     private Unit unit;
     private Date updateDateTime;
+
+    public Ingredient() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public Date getUpdateDateTime() {
+        return updateDateTime;
+    }
+
+    public void setUpdateDateTime(Date updateDateTime) {
+        this.updateDateTime = updateDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "\n  id=" + id +
+                ",\n name='" + name + '\'' +
+                ",\n unitPrice=" + unitPrice +
+                ",\n unit=" + unit +
+                ",\n updateDateTime=" + updateDateTime +
+                '}'+"\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return id == that.id && Objects.equals(unitPrice, that.unitPrice) && Objects.equals(name, that.name) && unit == that.unit && Objects.equals(updateDateTime, that.updateDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, unitPrice, unit, updateDateTime);
+    }
 }
