7bf0580332432f2c38c47442d0a03ca90adc0f7a
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-24 13:46:13 +0300

    feat: add RestoApplication.java file just show the list of ingredients

--- src/RestoApplication.java
+++ src/RestoApplication.java
@@ -1,5 +1,17 @@
+import dao.DishDao;
+import dao.IngredientDao;
+import entity.Dish;
+import entity.Ingredient;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class RestoApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException {
+
+
+        IngredientDao ingredientDao = new IngredientDao();
+        List<Ingredient> ingredients = ingredientDao.getAll(2,2);
+        System.out.println(ingredients);
     }
 }
