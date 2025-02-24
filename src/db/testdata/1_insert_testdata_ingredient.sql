018ad0ed1b81cc845c4c86a8aed0cc5b48c7e062
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-24 13:21:07 +0300

    feat: add file that insert data for my test ingredient

---
+++ src/db/testdata/1_insert_testdata_ingredient.sql
@@ -0,0 +1,5 @@
+INSERT INTO ingredient (id, name, unit_price, unit, update_datetime)
+    VALUES (1, 'Saucisse', 20, 'G', '2025-01-01'),
+           (2, 'Huile', 10000, 'L', '2025-01-01'),
+           (3, 'Oeuf', 1000, 'U', '2025-01-01'),
+           (4, 'Pain', 1000, 'U', '2025-01-01');
