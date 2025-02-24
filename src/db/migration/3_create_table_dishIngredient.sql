f26fdf3f941e04a27a7262f1f445d4ed2ab37c21
Johnathon47 <hei.johnathon.2@gmail.com>
2025-02-24 13:27:01 +0300

    feat: spelling mistakes on required_quantity

--- src/db/migration/3_create_table_dishIngredient.sql
+++ src/db/migration/3_create_table_dishIngredient.sql
@@ -2,7 +2,7 @@ CREATE TABLE if not exists dish_ingredient
     (
         id_dish INT,
         id_ingredient INT,
-        required_quality NUMERIC,
+        required_quantity NUMERIC,
         unit unit,
         PRIMARY KEY (id_dish, id_ingredient),
         FOREIGN KEY (id_dish) REFERENCES dish(id),
