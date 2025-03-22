8154cad5e048312a0026c49c19657c81f2b3ee6e
Johnathon47 <hei.johnathon.2@gmail.com>
2025-03-22 19:58:12 +0300

    feat: just change type of id in long

--- src/dao/CrudOperations.java
+++ src/dao/CrudOperations.java
@@ -6,6 +6,6 @@ public interface CrudOperations<E> {
 
     List<E> saveAll(List<E> entities); // Create if entities not exists, update if entities exists
     List<E> getAll(int offset, int limit);
-    E findById(int E_id);
-    boolean deleteOperation(int id);
+    E findById(long E_id);
+    boolean deleteOperation(long id);
 }
