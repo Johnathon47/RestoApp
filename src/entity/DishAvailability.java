af98c60adc5761a3972ab20f3b317593ad209ff7
Johnathon47 <hei.johnathon.2@gmail.com>
2025-03-22 20:02:32 +0300

    feat: create entity DishAvailabilty

---
+++ src/entity/DishAvailability.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.List;
+
+public class DishAvailability {
+    private Dish dish;
+    private Double availability;
+
+    /*public List<DishAvailability> getAllDishAvailability() {
+
+    }*/
+}
