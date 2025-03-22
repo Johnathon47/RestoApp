d495be2563b15a356df19f68ad13a475262172d4
Johnathon47 <hei.johnathon.2@gmail.com>
2025-03-22 19:56:16 +0300

    feat: request insert stock_movement

---
+++ src/db/testdata/4_insert_testdata_stock_movement.sql
@@ -0,0 +1,6 @@
+INSERT INTO stock_movement (id, ingredient_id, movement_type, quantity, unit, movement_date)
+VALUES
+  (1, 1, 'ENTRY', 10000, 'G', '2025-02-01 08:00:00'), -- Saucisse
+  (2, 2, 'ENTRY', 20, 'L', '2025-02-01 08:00:00'), -- Huile
+  (3, 3, 'ENTRY', 50, 'U', '2025-02-01 08:00:00'), -- Pain
+  (4, 4, 'ENTRY', 100, 'U', '2025-02-01 08:00:00'); -- Œuf
