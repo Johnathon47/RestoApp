11f9b2191f76e8ecd0706c4c4cbaf09c29099cbd
Johnathon47 <hei.johnathon.2@gmail.com>
2025-04-10 23:02:18 +0300

    feat: add getter, setter, toString, hashcode, equals and constructor

--- src/entity/StockMovement.java
+++ src/entity/StockMovement.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class StockMovement {
     private long id;
@@ -10,4 +11,88 @@ public class StockMovement {
     private BigDecimal quantity;
     private Unit unit;
     private Timestamp movementDate;
+
+    public StockMovement() {
+    }
+
+    public StockMovement(long id, Ingredient ingredient, MovementType movementType, BigDecimal quantity, Unit unit, Timestamp movementDate) {
+        this.id = id;
+        this.ingredient = ingredient;
+        this.movementType = movementType;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.movementDate = movementDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public MovementType getMovementType() {
+        return movementType;
+    }
+
+    public void setMovementType(MovementType movementType) {
+        this.movementType = movementType;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public Timestamp getMovementDate() {
+        return movementDate;
+    }
+
+    public void setMovementDate(Timestamp movementDate) {
+        this.movementDate = movementDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        StockMovement that = (StockMovement) o;
+        return id == that.id && Objects.equals(ingredient, that.ingredient) && movementType == that.movementType && Objects.equals(quantity, that.quantity) && unit == that.unit && Objects.equals(movementDate, that.movementDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ingredient, movementType, quantity, unit, movementDate);
+    }
+
+    @Override
+    public String toString() {
+        return "StockMovement{" +
+                "id=" + id +
+                ", ingredient=" + ingredient.getName() +
+                ", movementType=" + movementType +
+                ", quantity=" + quantity +
+                ", unit=" + unit +
+                ", movementDate=" + movementDate +
+                '}';
+    }
 }
